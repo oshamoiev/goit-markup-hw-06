cb8f4da67aae4572284d5a0aad3d63c2bc874b51
Oleksandr Shamoiev <wamoev@gmail.com>
2026-02-07 23:49:18 +0200

    Add mobile menu

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,20 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+
+    openMobileMenuBtn: document.querySelector('[data-mobile-menu-open]'),
+    closeMobileMenuBtn: document.querySelector('[data-mobile-menu-close]'),
+    mobileMenu: document.querySelector('[data-mobile-menu]'),
+
   }
 
-  refs.openModalBtn.addEventListener('click', toggleModal)
-  refs.closeModalBtn.addEventListener('click', toggleModal)
+  // refs.openModalBtn.addEventListener('click', () => toggleModal(refs.modal))
+  // refs.closeModalBtn.addEventListener('click', () => toggleModal(refs.modal))
 
-  function toggleModal () {
-    refs.modal.classList.toggle('is-open')
+  refs.openMobileMenuBtn.addEventListener('click', () => toggleModal(refs.mobileMenu))
+  refs.closeMobileMenuBtn.addEventListener('click', () => toggleModal(refs.mobileMenu))
+
+  function toggleModal (modal) {
+    modal.classList.toggle('is-open')
   }
 })()
